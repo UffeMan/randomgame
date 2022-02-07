7b4d3edb86f35befffa77a58f348844d60eed7f8
UffeMan <92825162+UffeMan@users.noreply.github.com>
2022-02-07 12:53:58 +0100

    First

---
+++ src/main/java/se/microsft/Game.java
@@ -0,0 +1,15 @@
+package se.microsft;
+
+class GameFactory {
+    public GF createRound(String) {
+        return new GF();
+    }
+}
+class GF {
+
+}
+public class Game {
+    public static void main(String[] args) {
+
+    }
+}
